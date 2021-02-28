b5cfdba9db65700d05e4d5d3fbd8048e4028c65e
spaceC00kie <kirstie317@gmail.com>
2021-02-27 20:15:12 -0500

    the About link is no longer visible on the About page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Footer = () => {
+  const [clicked, setClicked] = useState(false)
   return (
     <footer>
       <p>Copyright &copy; 2021</p>
-      <Link to="/about">About</Link>
+      { !clicked &&
+        <Link to="/about" onClick={() => {setClicked(true)}}>
+          About
+        </Link>
+      }
+      
     </footer>
   )
 }
